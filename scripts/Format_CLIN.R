1b4ad886d78475c9ec42913a746f3bdc5e1fe48f
Minoru Nakano <minoru.nakano@gmail.com>
2022-07-08 16:20:46 -0400

    update clin processing

--- scripts/Format_CLIN.R
+++ scripts/Format_CLIN.R
@@ -3,11 +3,13 @@ input_dir <- args[1]
 output_dir <- args[2]
 
 source("https://raw.githubusercontent.com/BHKLAB-Pachyderm/ICB_Common/main/code/Get_Response.R")
+source("https://raw.githubusercontent.com/BHKLAB-Pachyderm/ICB_Common/main/code/format_clin_data.R")
 
-clin = read.csv( file.path(input_dir, "CLIN.txt"), stringsAsFactors=FALSE , sep="\t" , dec=',')
-clin = clin[ clin$Arm %in% "NIVOLUMAB" , ]
+clin_original = read.csv( file.path(input_dir, "CLIN.txt"), stringsAsFactors=FALSE , sep="\t" , dec=',')
+clin = clin_original[ clin_original$Arm %in% "NIVOLUMAB" , ]
 
-clin = cbind( clin[ , c( "SUBJID" , "ORR" , "Sex" , "Age" , "OS" , "OS_CNSR" , "PFS..months." , "PFS_CNSR" ) ] , "PD-1/PD-L1" , "Kidney" , NA , NA , NA , NA , NA , NA )
+selected_cols <- c( "SUBJID" , "ORR" , "Sex" , "Age" , "OS" , "OS_CNSR" , "PFS..months." , "PFS_CNSR" )
+clin = cbind( clin[ , selected_cols ] , "PD-1/PD-L1" , "Kidney" , NA , NA , NA , NA , NA , NA )
 colnames(clin) = c( "patient" , "recist" , "sex"  ,"age"  , "t.os" , "os" , "t.pfs" , "pfs" , "drug_type" , "primary" , "response" , "response.other.info" , "histo" , "stage" , "dna" , "rna")
 
 clin$sex = ifelse(clin$sex %in% "Male" , "M" , "F" )
@@ -26,6 +28,8 @@ clin$dna[ clin$patient %in% case[ case$snv %in% 1 , ]$patient ] = "wes"
 
 rownames(clin) = clin$patient
 clin = clin[ case$patient , c("patient" , "sex" , "age" , "primary" , "histo" , "stage" , "response.other.info" , "recist" , "response" , "drug_type" , "dna" , "rna" , "t.pfs" , "pfs" , "t.os" , "os" ) ]
+
+clin = format_clin_data(clin_original, "SUBJID", selected_cols, clin)
 
 write.table( clin , file=file.path(output_dir, "CLIN.csv") , quote=FALSE , sep=";" , col.names=TRUE , row.names=FALSE )
 
