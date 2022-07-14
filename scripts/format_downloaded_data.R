66e9d3bfb2737efd60434f0d2924fe98463da2c5
Minoru Nakano <minoru.nakano@gmail.com>
2022-07-14 13:25:43 -0400

    modified function

--- scripts/format_downloaded_data.R
+++ scripts/format_downloaded_data.R
@@ -1,9 +1,10 @@
 library(readxl) 
+library(data.table)
 
 args <- commandArgs(trailingOnly = TRUE)
 work_dir <- args[1]
 
-format_excel <- function(input_path, output_path, sheetname, compress=FALSE){
+format_excel_and_save <- function(input_path, output_path, sheetname, compress=FALSE){
   df <- read_excel(input_path, sheet=sheetname)
   colnames(df) <- df[1, ]
   df <- df[-1, ]
@@ -17,20 +18,20 @@ format_excel <- function(input_path, output_path, sheetname, compress=FALSE){
   rm(df)
 }
 
-format_excel(
+format_excel_and_save(
   input_path=file.path(work_dir, '41591_2020_839_MOESM2_ESM.xlsx'),
   output_path=file.path(work_dir, 'CLIN.txt'),
   sheetname='S1_Clinical_and_Immune_Data'
 )
 
-format_excel(
+format_excel_and_save(
   input_path=file.path(work_dir, '41591_2020_839_MOESM2_ESM.xlsx'),
   output_path=file.path(work_dir, 'EXPR.txt.gz'),
   sheetname='S4A_RNA_Expression',
   compress=TRUE
 )
 
-format_excel(
+format_excel_and_save(
   input_path=file.path(work_dir, '41591_2020_839_MOESM2_ESM.xlsx'),
   output_path=file.path(work_dir, 'SNV.txt.gz'),
   sheetname='S2_WES_Data',
