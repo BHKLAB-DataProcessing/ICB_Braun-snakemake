ec7aaec68483298ae4a4810f6fb705ab1ef885d0
Minoru Nakano <minoru.nakano@gmail.com>
2022-07-18 13:04:57 -0400

    modified format_downloaded_data.R to import excel data processing functions

--- scripts/format_downloaded_data.R
+++ scripts/format_downloaded_data.R
@@ -4,19 +4,7 @@ library(data.table)
 args <- commandArgs(trailingOnly = TRUE)
 work_dir <- args[1]
 
-format_excel_and_save <- function(input_path, output_path, sheetname, compress=FALSE){
-  df <- read_excel(input_path, sheet=sheetname)
-  colnames(df) <- df[1, ]
-  df <- df[-1, ]
-  if(compress){
-    gz <- gzfile(output_path, "w")
-    write.table( df , file=gz , quote=FALSE , sep="\t" , col.names=TRUE , row.names=FALSE )
-    close(gz)
-  }else{
-    write.table( df , file=output_path , quote=FALSE , sep="\t" , col.names=TRUE , row.names=FALSE )
-  }
-  rm(df)
-}
+source('https://raw.githubusercontent.com/BHKLAB-Pachyderm/ICB_Common/main/code/format_excel_functions.R')
 
 format_excel_and_save(
   input_path=file.path(work_dir, '41591_2020_839_MOESM2_ESM.xlsx'),
